4389104c8a06247ac54e224805537361fa090587
Alexi Stein <lexi@stanford.edu>
2018-10-17 13:57:53 -0700

    Added swimming Eve to prefab, space bar controls swimming

---
+++ HowToMakeAVideoGame/Assets/Scripts/EveSwimming.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EveSwimming : MonoBehaviour {
+
+    public Animator anim;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)) {
+            anim.SetTrigger("isSwimming");
+            anim.ResetTrigger("isNotSwimming");
+        } else {
+            anim.ResetTrigger("isSwimming");
+            anim.SetTrigger("isNotSwimming");
+        }
+	}
+}
